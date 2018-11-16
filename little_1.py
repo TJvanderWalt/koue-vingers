98cfe698a671cd3bbf6aef653c33cd7269bb01c4
Outoppie TJ <vanderwalttjaartj@gmail.com>
2018-11-16 14:40:29 +0200

    Updated with solutions for CVhallenge 1 and 2

---
+++ little_1.py
@@ -0,0 +1,16 @@
+'''Write a program that will display a joke
+Don’t display the punchline until the reader
+hits the enter key.
+
+Extension
+display the punchline in a different colour'''
+
+import keyboard
+print("Like a joke? For the punchline, please press ENTER when you see ...")
+print("")  #to print an empty line
+print("It's the easiest of things for a married man to make plans for his vacation", "...", end='')
+key=input()
+print(math.pi)
+if len(key) >= 0:
+    print("the boss tells him when and the wife tells him where")
+#the two print statements above will print on the same line because of the end='' in the first print
---
+++ little_2.py
@@ -0,0 +1,17 @@
+'''Write a program that will ask you your name
+It will then display ‘Hello Name’ where ‘Name’ is the
+name you have entered.
+E.g.
+
+What is your name?
+Ada
+Hello Ada'''
+
+'''My attempt'''
+name=''
+print("What is your name?")
+name = input()
+print("Hello", name)
+
+'''Your attempt?
+First comment out my code and then add yours'''
